543a02b16b50717cc928040b67f22d3225887b5b
forrest <14351834@qq.com>
2016-02-01 11:40:05 +0800

    no message

--- InsuranceBroker/Base/BaseViewController.m
+++ InsuranceBroker/Base/BaseViewController.m
@@ -16,22 +16,6 @@
 #import "KGStatusBar.h"
 #import <AVOSCloud/AVOSCloud.h>
 
-
-//----------------- 定义网络变化单例参数
-static Reachability *_reachability = nil;
-static inline Reachability* defaultReachability () {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        _reachability = [Reachability reachabilityForInternetConnection];
-#if !__has_feature(objc_arc)
-        [_reachability retain];
-#endif
-    });
-    
-    return _reachability;
-}
-//--------------------- 定义网络变化单例参数
-
 @implementation BaseViewController
 
 
